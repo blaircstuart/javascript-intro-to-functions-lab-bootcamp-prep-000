67ad94d8708639f00624359c96ead178b13f4de7
Blair Stuart <blaircstuart28@gmail.com>
2018-09-13 13:53:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,5 +21,7 @@ if (string === string.toLowerCase){
 else if (string === string.toUpperCase()){
   return "YES INDEED!"
 }
-else if (string)
+else if (string === "I love you, Grandma."){
+  return "I love you, too."
+}
 }
