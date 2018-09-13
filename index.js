39e5ab84b89cd8236214f6861af25538967e076e
Blair Stuart <blaircstuart28@gmail.com>
2018-09-13 13:09:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-function shout(string){
-  return string.toUpperCase()
-}
-
-function whisper(string) {
-  return string.toLowerCase()
-}
-
-function logShout(string){
-  console.log(string.toUpperCase())
-}
-
-function logWhisper(string) {
-  console.log(string.toLowerCase()) 
-}
-
-
-var upperCase = upperCase.toUpperCase() === uppercase
-var lowerCase = lowerCase.toLowerCase() === lowerCase
-var mixedCase = 'Hi there!'
-
-mixedCase.toLowerCase() === mixedCase
-mixedCase.toUpperCase() === mixedCase
-
-function sayHiToGrandma(string)
-
-
 
 
 
